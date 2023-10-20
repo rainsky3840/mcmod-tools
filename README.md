c2919004d266c6b752b88f405d316882bbe058e7
Rainsky <kjh26895@gmail.com>
2023-10-20 21:14:53 +0900

    fix crash
    -fix potential crash when receiving wrong coordinate inputs

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 * currently supports planes and helicopters
 
 ## how to run
-* run main.py
+### If you have Python installed
+- run main.py
 > main.py and vec3d.py must be in the same folder
+### If Python is not installed
+* run the included .exe file
+> pre-compiled with pyinstaller
--- main.py
+++ main.py
@@ -17,17 +17,15 @@ pattern4 = re.compile(r"\w+\s*=\s*[\w\/\s-]+,\s*(?:false,\s*|true,\s*){1,3}([-+]
 # 5. Name = Parameter = 1, 0, (0.0, 0.0, 0.0)
 pattern5 = re.compile(r"\w+\s*=\s*[-+]?\d+\s*,\s*[-+]?\d+\s*,\s*([-+]?\d+\.?\d*,\s*[-+]?\d+\.?\d*,\s*[-+]?\d+\.?\d*)")
 
-#lists of parameters, patterns to loop
 searchlists = [searchlist1, searchlist2, searchlist3, searchlist4, searchlist5]
 patterns = [pattern1, pattern2, pattern3, pattern4, pattern5]
 headers = []
 
-#compile headers
 for i in range(len(searchlists)):
   header_new = re.compile(fr"^{'|'.join(searchlists[i])}\s*=")
   headers.append(header_new)
 
-#list to string function: ['a, b, c'] => [a, b, c] format
+#['a, b, c'] => [a, b, c]
 def list2str(found):
     all_list = []
     # all_str = []
@@ -52,7 +50,7 @@ def calc(vector):
     print(f'Calculated: {vector} => Move => [{v_result}]')
     return v_result
 
-print('▶▶ MCHELI CONFIG RESIZER - MADE BY RAINWIND ◀◀')
+print('▶▶ MCHELI RESIZER - MADE BY RAINWIND ◀◀')
 
 while True:
   try:
@@ -71,15 +69,21 @@ while True:
       ori = list(map(float, input('>> Origin (x,y,z): ').replace(' ', '').split(',')))
       scal = float(input('>> Scalar: ').replace(' ', ''))
       print('** Scaling numbers:', ori, scal)
+      if len(ori) != 3:
+        print('** Error! Please input three numbers separated by commas.')
+        continue
       break
     elif choice == '2':
       move = list(map(float, input('>> Move: ').replace(' ', '').split(',')))
       print('** Moving numbers:', move)
+      if len(move) != 3:
+        print('** Error! Please input three numbers separated by commas.')
+        continue
       break
     else:
       print('** Please choose between 1(Scale) and 2(Move)!')
   except:
-    print('** Error Encountered! Please input correctly.')
+    print('** Error! Please input correctly.')
 print('-'*30, end='\n\n')
 
 for line_idx, line in enumerate(text):
@@ -88,30 +92,21 @@ for line_idx, line in enumerate(text):
       print(f'Line {line_idx} : {line.strip()}')
       # print('Matching Header:', head.match(line).group())
       matches = ptn.findall(line)
-      
-      #single match: ['a,b,c'] / multiple matches: [('a,b,c','a,b,c')]
       if (matches[0]).__class__ == tuple:
           matches = list(matches[0])
-      
-      #format matches to [[a,b,c], [a,b,c]]
       matches = [i for i in matches if i != '']
-      # print(matches, type(matches))
       matches_list = list2str(matches)
       # print('Formatted:', matches, '=>', matches_list)
       
-      #get calculation results. stores multiple matches in one line.
       calc_results = []
       for idx, each in enumerate(matches_list):
         calc_result = calc(each) 
         calc_results.append(calc_result) 
       # print(f'Converted: {matches} => {calc_results}')
       
-      #replace old coordinates w/ new ones
       for (old_str, new_str) in zip(matches, calc_results):
         line = line.replace(old_str, new_str, 1)
         # print('LOOP:', line, end='')
-      
-      #push changes to main text body
       text[line_idx] = line
       print('FINAL:', line, end='')
       print()
